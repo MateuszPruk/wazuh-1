881bcea85721610868148db44cf7e6a81c5340b0
Lopuiz <lopezziur@gmail.com>
2020-07-10 11:29:51 +0200

    add limits to rule_test configuration

--- src/error_messages/debug_messages.h
+++ src/error_messages/debug_messages.h
@@ -238,4 +238,9 @@
 #define FIM_REALTIME_CALLBACK               "(6346): Realtime watch deleted for '%s'"
 #define FIM_DIR_RECURSION_LEVEL             "(6347): Directory '%s' is already on the max recursion_level (%d), it will not be scanned."
 
+/* wazuh-logtest debug messages*/
+#define LOGTEST_INV_NUM_THREADS             "(7000): Number of logtest threads too high. Only creates %d threads"
+#define LOGTEST_INV_NUM_USERS               "(7001): Number of maximum users connected in logtest too high. Only allows %d users"
+#define LOGTEST_INV_NUM_TIMEOUT             "(7002): Number of maximum user timeouts in logtest too high. Only allows %ds maximum timeouts"
+
 #endif /* DEBUG_MESSAGES_H */
